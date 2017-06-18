a112bf6257a7d7e0ed3a65d4d4dd2a9d60902338
Johnny Human <john.wennstrom@protonmail.com>
2017-06-18 13:30:03 +0200

    add mesh and fix scaling

--- src/js/chestahedron.js
+++ src/js/chestahedron.js
@@ -1,9 +1,14 @@
+/*
+ * Chestahedron has 7 Vertices and 7 Sides
+ * Vertices: A, B, C, P, Q, R, T
+ * Sides: AQTR, CRTP, BPTQ, ARC, BQA, BPC, ABC
+ */
+
 function Chestahedron(globals, options) {
     let defaults = {
-        baseTriangleSide: 1,
         radius: null,
         faceArea: null,
-        scale: 1
+        scale: 13
     }
 
     // protected
@@ -11,28 +16,28 @@ function Chestahedron(globals, options) {
     this.geometry = new THREE.BufferGeometry()
     this.solid = new THREE.Object3D()
 
-    // pivate
+    // private
     let theta = 94.8309261816304
     let thetaRad = theta / 180 * Math.PI
 
     globals.world.sceneAdd(this.solid)
 
-    this.baseTriangle = new EquiliteralTriangle({side: this.options.baseTriangleSide})
+    this.baseTriangle = new EquiliteralTriangle({side: this.options.scale})
 
-    // Calculate vertices A, B, C, P, Q, R, T
+    // Calculate vertices
     this.vertices = {}
     this.vertices.A = {
-        x: this.baseTriangle.radius('out') * this.options.scale,
+        x: this.baseTriangle.radius('out'),
         y: 0,
         z: 0
     }
     this.vertices.B = {
-        x: -this.baseTriangle.radius('in') * this.options.scale,
+        x: -this.baseTriangle.radius('in'),
         y: 0,
-        z: -this.options.baseTriangleSide / 2
+        z: -this.options.scale / 2
     }
     this.vertices.C = {
-        x: this.vertices.B.x * this.options.scale,
+        x: this.vertices.B.x,
         y: 0,
         z: -this.vertices.B.z
     }
@@ -43,13 +48,13 @@ function Chestahedron(globals, options) {
     }
     this.vertices.Q = {
         x: ((Math.sqrt(3) / 4) * ((1 / 3) - Math.cos(thetaRad))) * this.options.scale,
-        y: (Math.sqrt(3) / 2) * Math.sin(thetaRad),
+        y: (Math.sqrt(3) / 2) * Math.sin(thetaRad) * this.options.scale,
         z: (-(1 / 4) + (0.75 * Math.cos(thetaRad))) * this.options.scale
     }
     this.vertices.R = {
-        x: this.vertices.Q.x * this.options.scale,
-        y: this.vertices.Q.y * this.options.scale,
-        z: -this.vertices.Q.z * this.options.scale
+        x: this.vertices.Q.x,
+        y: this.vertices.Q.y,
+        z: -this.vertices.Q.z
     }
     this.vertices.T = {
         x: 0,
@@ -94,25 +99,68 @@ Chestahedron.prototype.drawLines = function () {
         []
     )
 
-    console.log(flatten([A, B, C, P, Q, R, T]))
-
-    this.addLine( Float32Array.from(flatten( [A, Q, T, R, A] )))
-    this.addLine( Float32Array.from(flatten( [A, B, C, A] )))
-    this.addLine( Float32Array.from(flatten( [T, P, B, Q] )))
-    this.addLine( Float32Array.from(flatten( [P, C, R] )))
+    this.addLine(Float32Array.from(flatten([A, Q, T, R, A])))
+    this.addLine(Float32Array.from(flatten([A, B, C, A])))
+    this.addLine(Float32Array.from(flatten([T, P, B, Q])))
+    this.addLine(Float32Array.from(flatten([P, C, R])))
 }
 
-Chestahedron.prototype.addLine = function(line) {
+Chestahedron.prototype.drawMesh = function () {
+    let geometry = new THREE.Geometry()
+    let V = this.vertices
+    let A = new THREE.Vector3(V.A.x, V.A.y, V.A.z)
+    let B = new THREE.Vector3(V.B.x, V.B.y, V.B.z)
+    let C = new THREE.Vector3(V.C.x, V.C.y, V.C.z)
+    let P = new THREE.Vector3(V.P.x, V.P.y, V.P.z)
+    let Q = new THREE.Vector3(V.Q.x, V.Q.y, V.Q.z)
+    let R = new THREE.Vector3(V.R.x, V.R.y, V.R.z)
+    let T = new THREE.Vector3(V.T.x, V.T.y, V.T.z)
+
+    let vec = [A, B, C, P, Q, R, T]
+    let faces = [
+        [2, 1, 0],
+        [2, 3, 1],
+        [2, 5, 3],
+        [0, 5, 2],
+        [0, 4, 5],
+        [1, 4, 0],
+        [1, 3, 4],
+        [5, 6, 3],
+        [3, 6, 4],
+        [4, 6, 5]
+
+    ]
+    for (let i = 0; i < vec.length; i++) {
+        geometry.vertices.push(vec[i])
+    }
+    for (let i = 0; i < faces.length; i++) {
+        console.log(new THREE.Face3(faces[i][0], faces[i][1], faces[i][2]))
+        geometry.faces.push(new THREE.Face3(faces[i][0], faces[i][1], faces[i][2]))
+    }
+    //geometry.computeFaceNormals()
+
+    var material = new THREE.MeshLambertMaterial({
+        opacity: 0.8,
+        transparent: true
+    });
+
+
+    this.solid.add(new THREE.Mesh(geometry, material))
+
+}
+
+Chestahedron.prototype.addLine = function (line) {
     let geometry = new THREE.BufferGeometry()
     geometry.addAttribute('position', new THREE.BufferAttribute(line, 3))
-    let material = new THREE.LineBasicMaterial({ color: 0x666666, linewidth: 0.002})
+    let material = new THREE.LineBasicMaterial({color: 0x666666, linewidth: 0.002})
     this.solid.add(new THREE.Line(geometry, material))
 }
 
 
 function EquiliteralTriangle(options) {
     let defaults = {
-        side: 1
+        side: 1,
+        area: null
     }
 
     this.options = globals.setDefaults(options, defaults)
@@ -129,4 +177,9 @@ EquiliteralTriangle.prototype.radius = function (position) {
         return (this.options.side / Math.sqrt(3)) / 2
     }
     return false
+}
+
+EquiliteralTriangle.prototype.area = function () {
+    let A = (3 * Math.sqrt(3) / 4) * Math.pow(this.options.side, 2)
+    return A
 }
